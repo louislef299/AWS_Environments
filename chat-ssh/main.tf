f6dacaa4c4c407a329452dc277a7af0a22aff8d9
Louis-Lefebvre <louislefebvre1999@gmail.com>
2021-09-06 15:04:22 -0500

    Add variables to the module for VPC configuration

--- chat-ssh/main.tf
+++ chat-ssh/main.tf
@@ -1,5 +1,12 @@
-module "environment" {
+module "ssh-chat" {
     source = "git::https://github.com/louislef299/terraform-aws-resources.git?ref=v1.0.0"
+
+    vpc_cidr = "10.0.0.0/16"
+    environment = "ssh-chat"
+
+    public_subnets = 1
+    private_subnets = 0
+    subnet_cidrs = ["10.0.1.0/24"]
 }
 
 // this needs to be done after you decide implementation 

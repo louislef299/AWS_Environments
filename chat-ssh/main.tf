eccb9f28d012ebbde45fdf40547cb348b81b6a33
Louis-Lefebvre <louislefebvre1999@gmail.com>
2021-09-18 12:42:33 -0500

    quick add

--- chat-ssh/main.tf
+++ chat-ssh/main.tf
@@ -3,13 +3,8 @@ module "ssh-chat-network" {
 
   environment = "ssh-chat"
   vpc-enabled = 1
-}
-
-module "ssh-chat" {
-  source = "git::https://github.com/louislef299/terraform-aws-resources.git"
 
   ec2-enabled = 1
   instance-regions = ["us-east-2"]
-  vpc_id = module.ssh-chat-netowrk.vpc.aws_vpc.default.id
 }
 // this needs to be done after you decide implementation 
